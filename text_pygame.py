479c1f791f18fc666eafe7b71c51917c45c38b69
horlas <aurelia.gourbere@gmail.com>
2018-04-03 13:25:05 +0200

    mouvements souris

--- text_pygame.py
+++ text_pygame.py
@@ -18,10 +18,11 @@ fenetre.blit(fond,(0,0))
 
 #Chargement et collage du personnage
 perso = pygame.image.load("perso.png").convert_alpha()
-position_perso = perso.get_rect()
-fenetre.blit(perso, position_perso)
-#position_perso_x = 0
-#position_perso_y = 0
+position_perso_x = 0
+position_perso_y = 0
+#position_perso = perso.get_rect()
+fenetre.blit(perso,(position_perso_x,position_perso_y))
+
 
 
 
@@ -36,25 +37,27 @@ while continuer:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			continuer = 0
-		if event.type == KEYDOWN:
-			if event.key == K_DOWN:
-				position_perso = position_perso.move(0,3)
-			if event.key == K_UP:
-				position_perso = position_perso.move(0,-3)
-			if event.key == K_LEFT:
-				position_perso = position_perso.move(-3,0)
-			if event.key == K_RIGHT:
-				position_perso = position_perso.move(3,0)		
+		#if event.type == KEYDOWN:
+			#if event.key == K_DOWN:
+			#	position_perso = position_perso.move(0,3)
+			#if event.key == K_UP:
+			#	position_perso = position_perso.move(0,-3)
+			#if event.key == K_LEFT:
+			#	position_perso = position_perso.move(-3,0)
+			#if event.key == K_RIGHT:
+			#	position_perso = position_perso.move(3,0)		
 		if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[1] < 100:
 			print("Zone dangereuse")
-		if event.type == MOUSEMOTION and event.buttons[0] == 1:
-			continuer = 0
-		if event.type == MOUSEMOTION and event.buttons[1] == 1:
+		#if event.type == MOUSEMOTION and event.buttons[0] == 1:
+			#continuer = 0
+		if event.type == MOUSEMOTION:
+				position_perso_x = event.pos[0]
+				position_perso_y = event.pos[1]
 
 
 
 	#Re-collage
 	fenetre.blit(fond,(0,0))
-	fenetre.blit(perso, position_perso)
+	fenetre.blit(perso,(position_perso_x, position_perso_y))
 	#Raffraichissement
 	pygame.display.flip()	
